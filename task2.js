5c9da84cd30386e12e12f69c149e834534186ce0
unknown <haneenshrateh@icloud.com>
2022-11-04 20:55:56 +0000

    JS

--- task2.js
+++ task2.js
@@ -1,26 +1,26 @@
-let x = prompt('operation')
-let y =prompt(`number1`)
-let z =prompt(`number2`)
+let x = prompt('operation');
+let y =Number(prompt('number1'));
+let z =Number(prompt('number2'));
 
 
-if (x ='+' )
-{document.write (Number(y)+ Number(z));
-alert (Number(y)+ Number(z));
-console.log (Number(y)+ Number(z));}
+if (x == '+' )
+{document.write (y+z);
+alert (y+z);
+console.log (y+z);
+}
 
-
-else if (x ='-')
+else if (x =='-')
 {document.write (y - z);
 alert (y - z);
 console.log (y - z);}
 
-else if (x = '*')
+else if (x == '*')
 {
     document.write (y * z);
     alert (y * z);
     console.log (y * z);  
 }
-else if (x = '/')
+else if (x == '/')
 {
     document.write (y / z);
     alert (y / z);
