b0529c650ff40ebf6fdb155edf6c0fc93deedfe2
unknown <haneenshrateh@icloud.com>
2022-11-04 19:35:53 +0000

    JS

---
+++ task2.js
@@ -0,0 +1,28 @@
+let x = prompt('operation')
+let y =prompt(`number1`)
+let z =prompt(`number2`)
+
+
+if (x ='+' )
+{document.write (Number(y)+ Number(z));
+alert (Number(y)+ Number(z));
+console.log (Number(y)+ Number(z));}
+
+
+else if (x ='-')
+{document.write (y - z);
+alert (y - z);
+console.log (y - z);}
+
+else if (x = '*')
+{
+    document.write (y * z);
+    alert (y * z);
+    console.log (y * z);  
+}
+else if (x = '/')
+{
+    document.write (y / z);
+    alert (y / z);
+    console.log (y / z); 
+}
---
+++ thursday03.11/task1.js
@@ -0,0 +1,15 @@
+let x = prompt ('x');
+let y = prompt ('y');
+
+if (x > y)
+{ document.write ('Hello World');
+  alert ('hello world');
+  console.log ('hello world');
+ }
+else
+{
+    document.write ('Goodbye');
+  alert ('Goodbye');
+  console.log ('Goodbye');
+
+}
